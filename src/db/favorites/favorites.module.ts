c8e57df70ca34f179ccf406eb7004a2024c25665
ITboo <annaacdc@tut.by>
2023-02-20 17:44:56 +0300

    feat: update favs

--- src/db/favorites/favorites.module.ts
+++ src/db/favorites/favorites.module.ts
@@ -8,13 +8,13 @@ import { ArtistsService } from '../artists/artists.service';
 import { AlbumsService } from '../albums/albums.service';
 import { TracksService } from '../tracks/tracks.service';
 
-import { Album } from '../albums/entities/album.entity';
 import { Artist } from '../artists/entities/artist.entity';
+import { Album } from '../albums/entities/album.entity';
 import { Track } from '../tracks/entities/track.entity';
 import { Favorites } from './entities/favorite.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Album, Track, Artist, Favorites])],
+  imports: [TypeOrmModule.forFeature([Artist, Album, Track, Favorites])],
   controllers: [FavoritesController],
   providers: [FavoritesService, ArtistsService, AlbumsService, TracksService],
 })
--- src/db/favorites/favorites.service.ts
+++ src/db/favorites/favorites.service.ts
@@ -5,17 +5,27 @@ import {
   NotFoundException,
   UnprocessableEntityException,
 } from '@nestjs/common';
-import { LocalDB } from '../storage';
-import { IFavouritesResponse } from './entities/favorite.interface';
+
 import { AlbumsService } from '../albums/albums.service';
 import { ArtistsService } from '../artists/artists.service';
 import { TracksService } from '../tracks/tracks.service';
+
 import {
   ALBUM_NOT_FOUND,
   ARTIST_NOT_FOUND,
   ERR,
   TRACK_NOT_FOUND,
 } from 'src/common/error';
+
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Artist } from '../artists/entities/artist.entity';
+import { Album } from '../albums/entities/album.entity';
+import { Track } from '../tracks/entities/track.entity';
+import { Favorites } from './entities/favorite.entity';
+
+const ID = 1;
 
 @Injectable()
 export class FavoritesService {
@@ -26,77 +36,107 @@ export class FavoritesService {
   @Inject(forwardRef(() => TracksService))
   private readonly tracksService: TracksService;
 
-  findAll() {
-    const result: IFavouritesResponse = {
-      artists: this.artistsService
-        .findAll()
-        .filter((item) => LocalDB.favourites.artists.includes(item.id)),
-      albums: this.albumsService
-        .findAll()
-        .filter((item) => LocalDB.favourites.albums.includes(item.id)),
-      tracks: this.tracksService
-        .findAll()
-        .filter((item) => LocalDB.favourites.tracks.includes(item.id)),
-    };
-    return result;
+  constructor(
+    @InjectRepository(Artist)
+    private artistRepository: Repository<Artist>,
+    @InjectRepository(Album)
+    private albumRepository: Repository<Album>,
+    @InjectRepository(Track)
+    private trackRepository: Repository<Track>,
+    @InjectRepository(Favorites)
+    private favoritesRepository: Repository<Favorites>,
+  ) {}
+
+  async findAll(): Promise<Favorites[]> {
+    return await this.favoritesRepository.find();
   }
 
-  addAlbum(id: string) {
-    try {
-      this.albumsService.findOne(id);
-      LocalDB.favourites.albums.push(id);
-    } catch (err) {
-      throw new UnprocessableEntityException(ERR);
-    }
-  }
-  addTrack(id: string) {
-    try {
-      this.tracksService.findOne(id);
-      LocalDB.favourites.tracks.push(id);
-    } catch (err) {
-      throw new UnprocessableEntityException(ERR);
+  async findOne(id = ID): Promise<Favorites> {
+    const favs = await this.favoritesRepository.findOne({
+      where: { userId: id },
+    });
+    if (!favs) {
+      throw new NotFoundException();
+    } else {
+      return favs;
     }
   }
 
-  addArtist(id: string) {
+  async addArtist(id: string) {
+    const favs = await this.favsHandler();
     try {
-      this.artistsService.findOne(id);
-      LocalDB.favourites.artists.push(id);
+      const artist = await this.artistsService.findOne(id);
+      favs.artists.push(artist);
     } catch (err) {
       throw new UnprocessableEntityException(ERR);
     }
   }
 
-  removeAlbum(id: string) {
-    const album = LocalDB.favourites.albums.find((el) => el === id);
+  async addAlbum(id: string) {
+    const favs = await this.favsHandler();
+    try {
+      const album = await this.albumsService.findOne(id);
+      favs.albums.push(album);
+    } catch (err) {
+      throw new UnprocessableEntityException(ERR);
+    }
+    await this.favoritesRepository.save(favs);
+  }
+  async addTrack(id: string) {
+    const favs = await this.favsHandler();
+    try {
+      const track = await this.tracksService.findOne(id);
+      favs.tracks.push(track);
+    } catch (err) {
+      throw new UnprocessableEntityException(ERR);
+    }
+    await this.favoritesRepository.save(favs);
+  }
+
+  async removeAlbum(id: string): Promise<void> {
+    const favs = await this.favsHandler();
+    const album = favs.albums.find((el) => el.id === id);
     if (album) {
-      LocalDB.favourites.albums = LocalDB.favourites.albums.filter(
-        (item) => item !== id,
-      );
+      favs.albums = favs.albums.filter((el) => el.id !== id);
+      await this.favoritesRepository.save(favs);
     } else {
       throw new NotFoundException(ALBUM_NOT_FOUND);
     }
+    await this.favoritesRepository.save(favs);
   }
 
-  removeArtist(id: string) {
-    const artist = LocalDB.favourites.artists.find((el) => el === id);
+  async removeArtist(id: string): Promise<void> {
+    const favs = await this.favsHandler();
+    const artist = favs.artists.find((el) => el.id === id);
     if (artist) {
-      LocalDB.favourites.artists = LocalDB.favourites.artists.filter(
-        (item) => item !== id,
-      );
+      favs.artists = favs.artists.filter((el) => el.id !== id);
+      await this.favoritesRepository.save(favs);
     } else {
       throw new NotFoundException(ARTIST_NOT_FOUND);
     }
   }
 
-  removeTrack(id: string) {
-    const track = LocalDB.favourites.tracks.find((el) => el === id);
+  async removeTrack(id: string): Promise<void> {
+    const favs = await this.favsHandler();
+    const track = favs.tracks.find((el) => el.id === id);
     if (track) {
-      LocalDB.favourites.tracks = LocalDB.favourites.tracks.filter(
-        (item) => item !== id,
-      );
+      favs.tracks = favs.tracks.filter((el) => el.id !== id);
+      await this.favoritesRepository.save(favs);
     } else {
       throw new NotFoundException(TRACK_NOT_FOUND);
     }
+  }
+
+  private favsObject = () => {
+    const favorites = new Favorites();
+    favorites.artists = [];
+    favorites.tracks = [];
+    favorites.albums = [];
+    return favorites;
+  };
+
+  async favsHandler() {
+    const favs = await this.findOne();
+    return favs ? favs : this.favsObject();
   }
 }
